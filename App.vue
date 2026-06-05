3a2a091d022ed043d07659d81148720c5191b51a
Simamkele daweti <simamkeledaweti32@gmail.com>
2026-06-05 23:19:51 +0200

    Add initial App.vue template for cooking courses

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header>
+      <h1>Cooking Masterclass</h1>
+      <div class="cart-icon">Cart: {{ totalItems }}</div>
+    </header>
+    
+    <div class="container">
+      <div class="courses">
+        <div class="course-card" v-for="course in courses" :key="course.id">
+          <img :src="course.image" :alt="course.name">
+          <div class="course-info">
+            <h3 class="course-name">{{ course.name }}</h3>
+            <div class="course-price">R{{ course.price }}</div>
+            <button class="add-btn" @click="addToCart(course)">Add to Cart</button>
+          </:
